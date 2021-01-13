c5afdddd636673b79d1bfd5cf9f50dc3fe38ceec
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-13 11:31:09 -0800

    commit fixed infinite loop, and now renders data

--- App.js
+++ App.js
@@ -5,28 +5,40 @@ import { StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
-    const [item, setItem] = useState([]);
-    useEffect(() => {
-      const proxyurl = "https://cors-anywhere.herokuapp.com/";
-      const url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
-      fetch(proxyurl + url)
-        .then(data => data.json())
-        .then((data) => {
-          console.log("this is data:", data);
-          setItem(data)
-          console.log("this is item:", item);
-        })
-        .catch((error) => {
-          setItem({error:error})
-        });
-    }, []);
+  const [item, setItem] = useState([]);
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  const url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
+
+  useEffect(() => {
+    fetch(proxyurl + url)
+    .then(data => data.json())
+    .then((data) => {
+      console.log("this is data:", data);
+      setItem(data)
+      console.log("this is item:", item);
+    })
+    .catch((error) => {
+      setItem({error:error})
+    });   
+  },[]);
     // const items = item.map(item =>(<Text key={ item.id }>{ item.id, item.listId, item.name }</Text>));
     const items = item.map((obj) => {
-      let id = item.id;
-      let listId = item.listId;
-      let name = item.name;
-      return <Text key={id}>{ id, listId, name }</Text>;
+      let id = obj.id;
+      let listId = obj.listId;
+      let name = obj.name;
+      // let listObj = [{
+      //   id:{id},
+      //   listId:{listId},
+      //   name:{name}
+      // }];
+      return <Text key={id}>[id:{id}, listId:{listId} name:{name}]</Text>;
+      // return <Text key={id}>{ listObj }</Text>;
     });
+
+    //  const format = items = Object.keys(items).reduce((results, key) => {
+    //    results.push({name:key, value:items[key] });
+    //    return results;
+    //  }, []);
 
     return (
     <View style={styles.container}>

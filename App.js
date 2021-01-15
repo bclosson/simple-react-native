f2ebda304a4907b70bddafbe131343e00f734422
benjaminclosson1 <benjaminclosson1@gmail.com>
2021-01-15 12:40:09 -0800

    commit activated api call

--- App.js
+++ App.js
@@ -4,179 +4,44 @@ import { StyleSheet, Text, View, FlatList, StatusBar} from 'react-native';
 
 class App extends React.Component {
       state = {
-        items: [
-          {
-            "id": 755,
-            "listId": 2,
-            "name": ""
-            },
-            {
-            "id": 203,
-            "listId": 2,
-            "name": ""
-            },
-            {
-            "id": 684,
-            "listId": 1,
-            "name": "Item 684"
-            },
-            {
-            "id": 276,
-            "listId": 1,
-            "name": "Item 276"
-            },
-            {
-            "id": 736,
-            "listId": 3,
-            "name": null
-            },
-            {
-            "id": 926,
-            "listId": 4,
-            "name": null
-            },
-            {
-            "id": 808,
-            "listId": 4,
-            "name": "Item 808"
-            },
-            {
-            "id": 599,
-            "listId": 1,
-            "name": null
-            },
-            {
-            "id": 424,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 444,
-            "listId": 1,
-            "name": ""
-            },
-            {
-            "id": 809,
-            "listId": 3,
-            "name": null
-            },
-            {
-            "id": 293,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 510,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 680,
-            "listId": 3,
-            "name": "Item 680"
-            },
-            {
-            "id": 231,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 534,
-            "listId": 4,
-            "name": "Item 534"
-            },
-            {
-            "id": 294,
-            "listId": 4,
-            "name": ""
-            },
-            {
-            "id": 439,
-            "listId": 1,
-            "name": null
-            },
-            {
-            "id": 156,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 906,
-            "listId": 2,
-            "name": "Item 906"
-            },
-            {
-            "id": 49,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 48,
-            "listId": 2,
-            "name": null
-            },
-            {
-            "id": 735,
-            "listId": 1,
-            "name": "Item 735"
-            },
-            {
-            "id": 52,
-            "listId": 2,
-            "name": ""
-            },
-            {
-            "id": 681,
-            "listId": 4,
-            "name": "Item 681"
-            },
-            {
-            "id": 137,
-            "listId": 3,
-            "name": "Item 137"
-            }
-        ], 
-        // loading: true,
+        items: [],    
+        loading: true,
       } 
   
-  // componentDidMount() {
-  //   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  //   const url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
+  componentDidMount() {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
 
-  //   fetch(proxyurl + url)
-  //     .then((buffer) => {
-  //       if (buffer.ok) {
-  //         return buffer.json(buffer)
-  //       } else {
-  //         console.log('API error')
-  //         this.setState({error: true})
-  //       }
-  //     })
-  //     .then((data) => {
-  //       console.log(data);
-  //       this.setState({
-  //         items: data,
-  //         loading: false,
-  //       })
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //       this.setState({error:error})
-  //     });
-  //}
+    fetch(proxyurl + url)
+      .then((buffer) => {
+        if (buffer.ok) {
+          return buffer.json(buffer)
+        } else {
+          console.log('API error')
+          this.setState({error: true})
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          items: data,
+          loading: false,
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+        this.setState({error:error})
+      });
+  }
 
 
   render() {
-    // if (this.state.loading) {
-    //   return <Text>Loading...</Text>
-    // } else if (this.state.error) {
-    //   return <Text>ERROR GETTING DATA</Text>
-    // } else {
+    if (this.state.loading) {
+      return <Text>Loading...</Text>
+    } else if (this.state.error) {
+      return <Text>ERROR GETTING DATA</Text>
+    } else {
       let items = this.state.items;
-      // const filtered = items.filter((item, i) => typeof items[i].name === 'string');
-      // const filtered1 = filtered.filter((item, i) => filtered[i].name !== '');
-      // const sorted1 = filtered1.sort((a, b) => (a.name > b.name ? 1 : -1)); 
-      // const sorted = sorted1.sort((a, b) => (a.listId > b.listId ? 1 : -1));
       const filtered = items.filter((item, i) => items[i].name !== null);
       const filtered1 = filtered.filter((item, i) => filtered[i].name !== '');
       const sorted = filtered1.sort((a, b) => {
@@ -215,7 +80,7 @@ class App extends React.Component {
         </View>
       )
     }
-  //}
+  }
 }
   const styles = StyleSheet.create({
       container: {
@@ -223,6 +88,8 @@ class App extends React.Component {
         backgroundColor: '#fff',
         alignItems: 'left',
         justifyContent: 'left',
+        margin: '0, 2%',
+        padding: '0, 0, 0, 10%'
       }
     });
 
